28e7adcb427c12af8669c3ecc0061fe73481270d
elmerspesia <elmer.dotti@spesia.com.br>
2025-05-12 19:24:28 -0300

    Create model.py

---
+++ .app/model.py
@@ -0,0 +1,18 @@
+import torch
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2 import model_zoo
+import streamlit as st
+
+@st.cache_resource
+def setup_predictor():
+    cfg = get_cfg()
+    cfg.merge_from_file(
+        model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
+    )
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(
+        "COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"
+    )
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    return DefaultPredictor(cfg)
